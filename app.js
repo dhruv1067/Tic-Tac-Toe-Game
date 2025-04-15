c38a33f451a8f6f13e3b555dcc3289cf95b999a0
dhruv1067 <dhruv67d@gmail.com>
2025-04-15 16:58:19 +0530

    winner condition added

--- app.js
+++ app.js
@@ -17,6 +17,31 @@ const winPatterns = [
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         console.log("box was clicked");
-        box.innerText ="abcd";
+        if(turn0){
+            box.innerText = `O`;
+            turn0 = false;
+        } else{
+            box.innerText = `X`;
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
     });
 });
+
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+            let pos1val = boxes[pattern[0]].innerText; 
+            let pos2val = boxes[pattern[1]].innerText; 
+            let pos3val = boxes[pattern[2]].innerText;
+    
+        if(pos1val != "" && pos2val != "" && pos3val != ""){
+            if(pos1val === pos2val && pos2val === pos3val){
+                console.log("Winner");
+            }
+        }
+
+    }
+}
